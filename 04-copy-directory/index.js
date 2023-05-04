8611bbc48ee3e7f24def2f1862a52cc4c7aa506c
Vladimir Sabaev <vladimir8sabaev@mail.ru>
2023-05-04 17:40:31 +0300

    feat: add 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,18 @@
+const fs = require('fs');
+const path = require('path');
+
+
+fs.rm(path.join(__dirname, 'files-copy'), { recursive: true },()=>{
+  fs.promises.mkdir(path.join(__dirname, 'files-copy'), {recursive: true})
+    .then(function(){
+      fs.promises.readdir(path.join(__dirname, 'files')).then(filenames => {
+        for (let filename of filenames) {
+          fs.promises.copyFile(path.join(__dirname, 'files', `${filename}`), path.join(__dirname, 'files-copy', `${filename}`));
+        }
+      });
+    });
+});
+
+
+
 
