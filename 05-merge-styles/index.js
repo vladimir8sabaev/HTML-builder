5c526af8de721148fc18aa69fcc5d4d834b23078
Vladimir Sabaev <vladimir8sabaev@mail.ru>
2023-05-05 10:38:52 +0300

    feat: add 05-merge-styles task

---
+++ 05-merge-styles/index.js
@@ -0,0 +1,19 @@
+const fs = require('fs');
+const path = require('path');
+const output = fs.createWriteStream(path.join(__dirname, 'project-dist', 'bundle.css'));
+fs.promises.readdir(path.join(__dirname, 'styles'),{withFileTypes: true})
+  .then(filenames => {
+    for (let filename of filenames) {
+      if(filename.isFile() && filename.name.split('.')[1] === 'css'){
+        const readableStream = fs.createReadStream(path.join(__dirname, 'styles', `${filename.name}`), 'utf-8');
+        readableStream.on('error', error => console.log('Error', error.message));
+        let data = '';
+        readableStream.on('data', chunk => data+= chunk);
+        // обезопасился от файлов большого размера, которые не влезут в один chunk
+        readableStream.on('end', () => output.write(data + '\n'));
+      }
+    }
+  })
+  .catch(err => {
+    console.log(err);
+  });
