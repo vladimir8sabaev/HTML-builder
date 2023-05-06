a0b46d1f8d1a9e159bf1ede59e1cbc91af5816ef
Vladimir Sabaev <vladimir8sabaev@mail.ru>
2023-05-06 14:39:43 +0300

    fix: use os.EOL instead of \n in task 02

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -1,19 +1,20 @@
 const fs = require('fs');
 const path = require('path');
+const os = require('os');
 const {stdin, stdout } = process;
 const readline = require('readline');
 process.on('exit', () => stdout.write('Bye, you are cool!'));
 
 const output = fs.createWriteStream(path.join(__dirname, 'text.txt'));
 const rl = readline.createInterface(stdin, stdout);
-
+const EOL = os.EOL;
 stdout.write('Hi, username! you can type anything you want!\n');
 
 rl.on('line', (input) => {
   if(input.trim() === 'exit'){
     process.exit();
   }
-  output.write(input + '\n');
+  output.write(`${input}${EOL}`);
 });
 
 process.on('SIGINT', () => process.exit());
