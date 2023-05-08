74604193eb3efec90bc57ff9c3ca8fe7822a8444
Vladimir Sabaev <vladimir8sabaev@mail.ru>
2023-05-08 18:40:05 +0300

    fix: wrap code in a copyDir() function in task 04

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,17 +1,19 @@
 const fs = require('fs');
 const path = require('path');
 
-
-fs.rm(path.join(__dirname, 'files-copy'), { recursive: true },()=>{
-  fs.promises.mkdir(path.join(__dirname, 'files-copy'), {recursive: true})
-    .then(function(){
-      fs.promises.readdir(path.join(__dirname, 'files')).then(filenames => {
-        for (let filename of filenames) {
-          fs.promises.copyFile(path.join(__dirname, 'files', `${filename}`), path.join(__dirname, 'files-copy', `${filename}`));
-        }
+function copyDir(){
+  fs.rm(path.join(__dirname, 'files-copy'), { recursive: true },()=>{
+    fs.promises.mkdir(path.join(__dirname, 'files-copy'), {recursive: true})
+      .then(function(){
+        fs.promises.readdir(path.join(__dirname, 'files')).then(filenames => {
+          for (let filename of filenames) {
+            fs.promises.copyFile(path.join(__dirname, 'files', `${filename}`), path.join(__dirname, 'files-copy', `${filename}`));
+          }
+        });
       });
-    });
-});
+  });
+}
+copyDir();
 
 
 
